be852ad2a41ee410b4073432b9a370dc20dadd40
Daniel Johnson <dsaved8291@gmail.com>
2025-02-16 11:13:32 +0100

    Remove dir dep now using git url

--- lib/agora_rtm_1.5/ios/Classes/AgoraRtmPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface AgoraRtmPlugin : NSObject<FlutterPlugin>
-@end
--- lib/agora_rtm_1.5/lib/agora_rtm.dart
+++
@@ -1,4 +0,0 @@
-export 'src/agora_rtm_call_manager.dart';
-export 'src/agora_rtm_channel.dart';
-export 'src/agora_rtm_client.dart';
-export 'src/utils.dart';
